a60dc11ec3de16cbeb7bc3127506708f8cfb07b9
Luiz Waldrich <luiz.waldrich@gmail.com>
2018-02-04 22:20:38 -0200

    Criado os repositorios e entities

---
+++ src/main/java/com/udemy/pontointeligente/api/utils/PasswordUtils.java
@@ -0,0 +1,28 @@
+package com.udemy.pontointeligente.api.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+
+	private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+	
+
+	public PasswordUtils() {
+		
+	}
+	
+	public static String gerarBCrypt(String senha) {
+		if (senha == null) {
+			return senha;
+		}
+		
+		log.info("Gerando hash com o BCrypt");
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.encode(senha);
+	}
+	
+	
+
+}
